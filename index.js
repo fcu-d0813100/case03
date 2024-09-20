ca02b38f9fc416db1685524fe482768ab449d239
wie <you@example.com>
2024-09-20 11:34:19 +0800

    test_indexJS

---
+++ index.js
@@ -0,0 +1,6 @@
+const uniq = require("uniq");
+const arr = [2, 4, 5, 2, 3, 4, 5, 3, 13, 51, 3514, 351, 351, 3, 4];
+const result = uniq(arr);
+console.log(result);
+console.log(process.argv[2]);
+console.log("NMSL");
